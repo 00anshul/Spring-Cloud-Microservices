269874750fb1f50b45da356571f5c7fe15676af2
Anshul Sharma <sharmaanshul7018@gmail.com>
2026-03-14 15:19:49 +0530

    circuit breaker,rabbitmq

--- Project-order-service/src/main/java/com/order/service/service/OrderService.java
+++ Project-order-service/src/main/java/com/order/service/service/OrderService.java
@@ -7,6 +7,8 @@ import com.order.service.dto.ProductDTO;
 import com.order.service.entity.Order;
 import com.order.service.repository.OrderRepository;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class OrderService {
 
@@ -16,6 +18,7 @@ public class OrderService {
     @Autowired
     private ProductClient productClient;
 
+    @CircuitBreaker(name="product-service",fallbackMethod="createOrderFallback")
     public Order createOrder(Order order) {
         // Step 1 — Fetch product details from Product Service
         ProductDTO product = productClient.getProductById(order.getProduct_id());
@@ -26,5 +29,13 @@ public class OrderService {
 
         // Step 3 — Save and return
         return orderRepo.save(order);
+    }
+    public Order createOrderFallback(Order order, Throwable ex) {
+        Order fallbackOrder = new Order();
+//        fallbackOrder.setProduct_id(order.getProduct_id());
+//        fallbackOrder.setProduct_quantity(order.getProduct_quantity());
+//        fallbackOrder.setProduct_price(0);
+        fallbackOrder.setPro_desc("Product Service is currently unavailable. Please try again later.");
+        return fallbackOrder;
     }
 }
